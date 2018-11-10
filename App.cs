0aa27b6ee4a1840136d3b00766b9bf21bd5d9451
Deyan Nenov <dnenov@archilizer.com>
2018-11-10 16:40:00 +0000

    many changes

--- App.cs
+++ App.cs
@@ -30,22 +30,22 @@ namespace Archilizer_Purge
             // Get dll assembly path
             string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
 
-            PushButtonData delBackupsBtn = CreatePushButton("Delete Backups", "Delete Backups", thisAssemblyPath, "Archilizer_Purge.CommandDeleteBackups",
+            PushButtonData delBackupsBtn = CreatePushButton("Delete Backups", "1. Delete Backups", thisAssemblyPath, "Archilizer_Purge.CommandDeleteBackups",
                 "Deletes those pesky .00??.rvt files.", "purge.png", "purge_small.png");
 
-            PushButtonData dwgBtn = CreatePushButton("Imported DWG Files", "Imported DWG Files", thisAssemblyPath, "Archilizer_Purge.PurgeImportedDWG", 
+            PushButtonData dwgBtn = CreatePushButton("Imported DWG Files", "2. Imported DWG Files", thisAssemblyPath, "Archilizer_Purge.PurgeImportedDWG", 
                 "Purges imported (but not linked) DWG files in the current project.", "purge.png", "purge_small.png");
 
-            PushButtonData dwgLinesBtn = CreatePushButton("DWG Line Patterns", "DWG Line Patterns", thisAssemblyPath, "Archilizer_Purge.PurgeImportedLinePatterns",
+            PushButtonData dwgLinesBtn = CreatePushButton("DWG Line Patterns", "3. DWG Line Patterns", thisAssemblyPath, "Archilizer_Purge.PurgeImportedLinePatterns",
                 "Purges line patterns brought by imported DWG files.", "purge.png", "purge_small.png");
 
-            PushButtonData viewsBtn = CreatePushButton("Views not on Sheets", "Views not on Sheets", thisAssemblyPath, "Archilizer_Purge.CommandViewsNotOnSheets",
+            PushButtonData viewsBtn = CreatePushButton("Views not on Sheets", "4. Views not on Sheets", thisAssemblyPath, "Archilizer_Purge.CommandViewsNotOnSheets",
                 "Purges Views not placed on Sheets. Use at your own risk!", "purge.png", "purge_small.png");
 
-            PushButtonData vtemplateBtn = CreatePushButton("Unused View Templates", "Unused View Templates", thisAssemblyPath, "Archilizer_Purge.PurgeUnusedTemplates",
+            PushButtonData vtemplateBtn = CreatePushButton("Unused View Templates", "5. Unused View Templates", thisAssemblyPath, "Archilizer_Purge.PurgeUnusedTemplates",
                 "Purges Unused View Templates.", "purge.png", "purge_small.png");
 
-            PushButtonData filtersBtn = CreatePushButton("Unused Filters", "Unused Filters", thisAssemblyPath, "Archilizer_Purge.PurgeFilters",
+            PushButtonData filtersBtn = CreatePushButton("Unused Filters", "6. Unused Filters", thisAssemblyPath, "Archilizer_Purge.PurgeFilters",
                 "Purges Unused Filters (Filters that have never been assigned to a View or a View Template).", "purge.png", "purge_small.png");
 
             PulldownButtonData pd1 = new PulldownButtonData("purgeButton", "Purge");
